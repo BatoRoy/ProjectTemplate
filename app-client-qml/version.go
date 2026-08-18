055b296499552d0dffa4d11731f082297f88d525
BatoRoy <kaptenroy00@gmail.com>
2026-08-18 07:25:58 +0200

    chore: bump version to 0.3.0

--- app-client-qml/version.go
+++ app-client-qml/version.go
@@ -2,4 +2,4 @@ package main
 
 // Overwritten at build time with -ldflags="-X main.Version=$(VERSION)" and kept
 // in sync with the repo's VERSION file by version.sh.
-var Version = "0.2.0"
+var Version = "0.3.0"
